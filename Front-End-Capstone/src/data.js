8b5752497622355ab6994dd24c4d155f6a52616b
allenyonemoto <167946824+allenyonemoto@users.noreply.github.com>
2024-05-29 17:34:51 +0900

    Add files via upload

---
+++ Front-End-Capstone/src/data.js
@@ -0,0 +1,89 @@
+import appe1 from "./images/menu/crabartichoke.jpeg";
+import appe2 from "./images/menu/spinachballs.jpg";
+import appe3 from "./images/menu/vegannachos.jpeg";
+import main1 from "./images/menu/couscous.jpg";
+import main2 from "./images/menu/mezze.jpg";
+import main3 from "./images/menu/paella.jpg";
+import main4 from "./images/menu/Ratatouille.jpg";
+import main5 from "./images/menu/risotto.jpg";
+import main6 from "./images/menu/Shakshuka.jpg";
+import des1 from "./images/menu/cremebrulee.jpeg";
+import des2 from "./images/menu/galaktoboureko.jpg";
+import des3 from "./images/menu/namoura.jpg";
+
+const menu = [
+  {
+    type: "appetizer",
+    name: "Crab Artichoke",
+    image: appe1,
+    price: "$9.99",
+  },
+  {
+    type: "appetizer",
+    name: "Spinach Balls",
+    image: appe2,
+    price: "$5.99",
+  },
+  {
+    type: "appetizer",
+    name: "Vegan Nachos",
+    image: appe3,
+    price: "$7.99",
+  },
+  {
+    type: "main",
+    name: "Couscous",
+    image: main1,
+    price: "$10.99",
+  },
+  {
+    type: "main",
+    name: "Mezze",
+    image: main2,
+    price: "$10.99",
+  },
+  {
+    type: "main",
+    name: "Paella",
+    image: main3,
+    price: "$14.99",
+  },
+  {
+    type: "main",
+    name: "Ratatouille",
+    image: main4,
+    price: "$11.99",
+  },
+  {
+    type: "main",
+    name: "Risotto",
+    image: main5,
+    price: "$12.99",
+  },
+  {
+    type: "main",
+    name: "Shakshuka",
+    image: main6,
+    price: "$12.99",
+  },
+  {
+    type: "dessert",
+    name: "Crema Catalana",
+    image: des1,
+    price: "$4.99",
+  },
+  {
+    type: "dessert",
+    name: "Galaktoboureko",
+    image: des2,
+    price: "$5.99",
+  },
+  {
+    type: "dessert",
+    name: "Namoura",
+    image: des3,
+    price: "$4.99",
+  },
+];
+
+export default menu;
